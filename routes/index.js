892407b96d365f8a2a6d6ac3bc8c8bd52ef98fce
Travis Lawrence <travis.lawrence12@gmail.com>
2023-12-26 17:19:58 -0800

    add search endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.get("/codes", async function (req, res, next) {
+let smallparsed;
+let parsed;
+const loadFiles = async () => {
+  if (smallparsed || parsed) {
+    return { smallparsed, parsed };
+  }
   const codePath = path.join(
     __dirname,
     "../public",
@@ -15,12 +16,39 @@ router.get("/codes", async function (req, res, next) {
     "json",
     "IPT Codes - Codes2 icd10cm_order_2024.json"
   );
+  const smallCodePath = path.join(
+    __dirname,
+    "../public",
+    "cpt_codes",
+    "json",
+    "IPT Codes - Codes3 icd10pcs_codes_2024.json"
+  );
+
+  const smallcodes = await fs.promises.readFile(smallCodePath);
+  const smallresp = await smallcodes.toString();
+  smallparsed = JSON.parse(smallresp);
 
   const codes = await fs.promises.readFile(codePath);
   const resp = await codes.toString();
-  const parsed = JSON.parse(resp);
+  parsed = JSON.parse(resp);
 
-  const smallCodes = parsed.slice(0, 100);
+  return { smallparsed, parsed };
+};
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/search", async function (req, res, next) {
+  await loadFiles();
+  const { keyword } = req.query;
+  const filtered = parsed.filter((p) => p["Description"].includes(keyword));
+  res.json(filtered);
+});
+
+router.get("/codes", async function (req, res, next) {
+  const files = await loadFiles();
+  const smallCodes = files.parsed.slice(0, 100);
   res.json(smallCodes);
 });
 
